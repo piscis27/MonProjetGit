e23cf67a2f068e10eef3a531b236e0c3fe2f693f
Yvan <yvancollin@gmail.com>
2020-11-19 10:20:29 +0100

    premiére version du projet GIT

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>ProjetGit</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="alert alert-info display-4 text-center my-5 shadow-lg">Test !!</h1> 
+    </div>
+</body>
+</html>
